5ae90791dcf68b08423dac4bac5ef3b98fc521e9
DmLebedev <lebedevdmitry7660882@gmail.com>
2024-06-01 21:13:37 +0300

    feat: projectile

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// App.js
 import React, { useState, useEffect } from "react";
 import styles from "./App.module.css";
 
@@ -18,12 +19,41 @@ const Enemy = ({ id, x, y, health, removeEnemy }) => {
   );
 };
 
+const Projectile = ({ id, x, y, removeProjectile }) => {
+  const [position, setPosition] = useState({ x, y });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPosition((prevPosition) => ({
+        x: prevPosition.x + 1,
+        y: prevPosition.y,
+      }));
+    }, 1);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (position.x >= window.innerWidth || position.y >= window.innerHeight) {
+      removeProjectile(id);
+    }
+  }, [position.x, position.y, removeProjectile, id]);
+
+  return (
+    <img
+      src="projectile.gif"
+      alt="projectile"
+      className={styles.projectile}
+      style={{ top: `${position.y}px`, left: `${position.x}px` }}
+    />
+  );
+};
+
 const App = () => {
   const [enemies, setEnemies] = useState([]);
+  const [projectiles, setProjectiles] = useState([]);
   const [heroHealth, setHeroHealth] = useState(100);
   const [isGameOver, setIsGameOver] = useState(false);
-
-  const windowWidth = window.innerWidth > 730 ? 600 : window.innerWidth - 100;
 
   useEffect(() => {
     if (heroHealth === 0) {
@@ -37,10 +67,10 @@ const App = () => {
         if (prevEnemies.length === 0) return prevEnemies;
 
         const updatedEnemies = prevEnemies.map((enemy, index) => {
-          if (enemy.x >= windowWidth && index !== 0) {
+          if (enemy.x >= window.innerWidth && index !== 0) {
             return enemy;
           }
-          if (enemy.x >= windowWidth && index === 0) {
+          if (enemy.x >= window.innerWidth && index === 0) {
             return {
               ...enemy,
               health: enemy.health - (isGameOver ? 0 : 10),
@@ -88,18 +118,45 @@ const App = () => {
       if (
         enemies.some(
           (enemy) =>
-            enemy.x >= (window.innerWidth > 730 ? windowWidth : windowWidth + 5)
+            enemy.x >=
+            (window.innerWidth > 730
+              ? window.innerWidth - 100
+              : window.innerWidth - 95)
         )
       ) {
         clearInterval(interval);
         setHeroHealth((prevHealth) => prevHealth - 5);
       }
     }, 2000);
+    return () => clearInterval(interval);
   }, [enemies]);
 
   const removeEnemy = (id) => {
     setEnemies((prevEnemies) => prevEnemies.filter((enemy) => enemy.id !== id));
   };
+
+  const removeProjectile = (id) => {
+    setProjectiles((prevProjectiles) =>
+      prevProjectiles.filter((projectile) => projectile.id !== id)
+    );
+  };
+
+  const throwProjectile = () => {
+    const newProjectile = {
+      id: Date.now(),
+      x: 100,
+      y: 550,
+    };
+    setProjectiles((prevProjectiles) => [...prevProjectiles, newProjectile]);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      throwProjectile();
+    }, 1000); // Выстрел каждую секунду (1000 миллисекунд)
+
+    return () => clearInterval(interval); // Очистка интервала при размонтировании компонента
+  }, []);
 
   return (
     <div className={styles.outerWrapper}>
@@ -118,6 +175,13 @@ const App = () => {
         <img src="player.png" alt="player" className={styles.hero} />
         {enemies.map((enemy) => (
           <Enemy key={enemy.id} {...enemy} removeEnemy={removeEnemy} />
+        ))}
+        {projectiles.map((projectile) => (
+          <Projectile
+            key={projectile.id}
+            {...projectile}
+            removeProjectile={removeProjectile}
+          />
         ))}
       </div>
     </div>
