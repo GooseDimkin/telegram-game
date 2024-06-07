5fe8131b9fe5addbf25ce85f7a747a56ccf8e560
DmLebedev <lebedevdmitry7660882@gmail.com>
2024-06-07 12:40:53 +0300

    feat: projectiles component

---
+++ src/components/projectiles/projectile/projectile.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import styles from './projectile.module.css'
+
+const Projectile = ({
+  id,
+  x,
+  y,
+  removeProjectile,
+  enemies,
+  setIsProjectileHit,
+}) => {
+  const [position, setPosition] = useState({ x, y });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPosition((prevPosition) => ({
+        x: prevPosition.x + 1,
+        y: (enemies[0] && enemies[0].y) + 3,
+      }));
+    }, 1);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  let enemiesCollision =
+    window.innerWidth > 730
+      ? 760 - (enemies[0] && enemies[0].x)
+      : 400 - (enemies[0] && enemies[0].x);
+
+  useEffect(() => {
+    if (position.x >= enemiesCollision || position.y >= window.innerHeight) {
+      setIsProjectileHit(true);
+      removeProjectile(id);
+    }
+  }, [position.x, position.y, removeProjectile, id]);
+
+  return (
+    <img
+      src="projectile.gif"
+      alt="projectile"
+      className={styles.projectile}
+      style={{ top: `${position.y}%`, left: `${position.x}px` }}
+    />
+  );
+};
+
+export default Projectile;
