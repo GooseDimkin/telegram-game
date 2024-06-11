983ef31308ffeffc72ce3799fb453e256fbfffb7
DmLebedev <lebedevdmitry7660882@gmail.com>
2024-06-11 17:40:05 +0300

    feat: vawes

--- src/components/enemies/enemies.tsx
+++ src/components/enemies/enemies.tsx
@@ -2,11 +2,22 @@ import React from "react";
 import Enemy from "./enemy/enemy";
 import { IEnemies, IEnemy } from "src/interface/interface";
 
-const Enemies: React.FC<IEnemies> = ({ enemies, removeEnemy }: IEnemies) => {
+const Enemies: React.FC<IEnemies> = ({
+  enemies,
+  removeEnemy,
+  skin,
+  currentVawe,
+}: IEnemies) => {
   return (
     <div>
       {enemies.map((enemy: IEnemy) => (
-        <Enemy key={enemy.id} {...enemy} removeEnemy={removeEnemy} />
+        <Enemy
+          key={enemy.id}
+          {...enemy}
+          removeEnemy={removeEnemy}
+          skin={skin}
+          currentVawe={currentVawe}
+        />
       ))}
     </div>
   );
